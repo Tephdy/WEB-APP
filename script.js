aa83dbb4ee9ec9a241918d7ae18f809e2b72a88a
Joseph Tanay Amandy <53117182+Tephdy@users.noreply.github.com>
2025-09-11 14:50:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const properties = {
       "6,298 PER MONTH 3 MONTHS CONTRACT (3000) UTILITY DEPOSIT.",
       "6,888 PER MONTH MONTHL CONTRACT 1 MONTH ADVANCE.",
     ],
-    landmark:"Boundary of Pasig and Cainta. Walking distance to Ortigas Extension & Robinson's (Big R) Cainta Junction. Near the business district of Ortigas, Makati, Eastwood and Cubao.",
+    landmark:"Boundary of Pasig and Cainta. Walking distance to Ortigas Extension & Robinson's (Big R) Cainta Junction. Near the business district of Ortigas, Makati, Eastwood and Cubao. Google Map: https://maps.app.goo.gl/ZtoYCfw6VS1Q2Tiy7",
   },
   "ADI 168": {
     name: "ADI 168",
