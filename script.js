33ae14a259c8ab85adcdbe7cfe79726b04efb69b
Joseph Tanay Amandy <53117182+Tephdy@users.noreply.github.com>
2025-09-22 11:13:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,196 +1,234 @@
-const properties = {
-  "ECO (888)": {
-    name: "ECO (888)",
-    type: "Studio Unit",
-    location: "69 Matahimik Street Riverside 2 Subdivision Brgy. Sto. Domingo Cainta, Rizal.",
-    map: "https://maps.app.goo.gl/ZtoYCfw6VS1Q2Tiy7",
-    contracts: [
-      "5,399 PER MONTH 1 YEAR CONTRACT.<br> 1 MONTH ADVANCE 2 MONTHS DEPOSIT.",
-      "5,899 PER MONTH 6 MONTHS CONTRACT.<br> 1 MONTH ADVANCE 2 MONTHS DEPOSIT.",
-      "6,298 PER MONTH 3 MONTHS CONTRACT (3000) UTILITY DEPOSIT.",
-      "6,888 PER MONTH MONTHLY CONTRACT 1 MONTH ADVANCE.",
-    ],
-    landmark: "Boundary of Pasig and Cainta. Walking distance to Ortigas Extension & Robinson's (Big R) Cainta Junction. Near Ortigas, Makati, Eastwood and Cubao.",
-  },
-  "ADI 168": {
-    name: "ADI 168",
-    type: "Semi-furnished unit",
-    location: "Lt. 6 Blk. 15 Kayumangi St. Karangalan Village, San Isidro Cainta, Rizal",
-    map: "https://maps.app.goo.gl/EQkAxPWgDAhyknM79",
-    contracts: [
-      "Trial - 3,999 - 1 MONTH - 1k utility DEPOSIT",
-      "3,999 PER MONTH - 6 MONTH CONTRACT - 1 MONTH ADVANCE 1 MONTH DEPOSIT"
-    ],
-    landmark: "Walking distance to LRT Line 2 Marikina-Pasig Station. Near Cubao, Ortigas, Makati, and Eastwood.",
-  },
-  "BED AND BATH": {
-    name: "Bed and Bath",
-    type: "Bedspace unit",
-    location: "74 Monaco St, Ciudad Grande 2, Pasig City, 1611 Metro Manila",
-    map: "https://maps.app.goo.gl/hphkFhzp1XebbM849",
-    contracts: [
-      "299 - Daily Minimum of 3 days stay.",
-      "1 MONTH TRIAL -(Lower bed: 2,200, Upper bed: 1,999)",
-      "Extended 1 month trial (Lower bed: 2,500, Upper bed: 2,300)",
-      "3,100 - MONTHLY CONTRACT",
-    ],
-    landmark: "One ride to Ortigas, Mandaluyong, Makati, Eastwood, Libis, and BGC. Walking distance to SM East Ortigas, Super 8 Grocery, McDonald's, and banks.",
-  },
-  "DREAM": {
-    name: "DREAM",
-    type: "Studio Unit",
-    location: "80 Monaco St. Cuidad Grande 2, Ortigas Extension Pasig City.",
-    map: "https://maps.app.goo.gl/AP35c9sccbpSmaqU8",
-    contracts: [
-      "1 BEDROOM - 10,200 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "10,700 PER MONTH - 6 MONTHS CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "11,200 PER MONTH - MONTHLY CONTRACT - 3000 UTILITY DEPOSIT",
-      "1,000 - DAILY RENTAL",
-      "2 BEDROOMS - 12,400 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "12,900 PER MONTH - 6 MONTHS CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "13,400 - MONTHLY - 3K UTILITY DEPOSIT",
-      "1,000 - DAILY",
-      "STUDIO - 7,900 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "8,400 PER MONTH - 6 MONTHS CONTACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "8,900 PER MONTH - MONTHLY CONTRACT - 3K UTILITY DEPOSIT",
-      "Studio unit",
-      "700 - DAILY"
-    ],
-    landmark: "Walking distance from SM East Ortigas. Near Ortigas, Makati, BGC, Eastwood, Cubao, and Marikina.",
-  },
-  "PLEASANT": {
-    name: "PLEASANT",
-    type: "Studio unit",
-    location: "Mt. Pleasant St. Brgy. Sta. Elena Marikina City",
-    map: "https://maps.app.goo.gl/5D3Ke41BDTLFLHPh9",
-    contracts: [
-      "1A TO 1E - 4,999 PER MONTH - 1 YR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "5,400 PER MONTH - 6 MONTHS CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "5,999 - MONTHLY CONTRACT - 3K UTILITY DEPOSIT",
-      "700 - DAILY",
-      "UNIT 1F, 2L, 3R, AND 4X - 5,099 PER MONTH - 1 YR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "5,599 PER MONTH - 6 MONTHS CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "6,099 - MONTHLY CONTRACT - 3K UTILITY DEPOSIT"
-    ], 
-    landmark: "Near LRT Line 2 Marikina-Pasig Station. 1 ride to Cubao, Makati, Ortigas, Eastwood, and Manila.",
-  },
-  "HOMEY": {
-    name: "HOMEY",
-    type: "Bedroom unit",
-    location: "Lot 1 Block. 3 Kanlaon St., DM 5, Cainta, Rizal",
-    map: "https://maps.app.goo.gl/VnyT7tHi4QXXRHXj9",
-    contracts: [
-      "1ST FLOOR FRONT STUDIO - 3,999 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "2ND FLOOR 1 BEDROOM - 6,000 PER MONTH - 1 YR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "3,4,5 - 5,500 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "#2 - 6,000 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
-      "6,7,8 - 6,000 PER MONTH – 1 YEAR CONTRACT W/ AC PROVISION - 1 MONTH ADVANCE 2 MONTHS DEPOSIT"
-    ],
-    landmark: "Near Cainta Municipal Hall, SM Savemore, Puregold, markets. Accessible to Ortigas Ext., C6, BGC, and Pasig.",
-  },
-  "KALAYAAN 888": {
-    name: "KALAYAAN 888",
-    type: "Studio unit",
-    location: "Karangalan Gate 1 Kalayaan St. San Isidro Cainta, Rizal",
-    map: "https://maps.app.goo.gl/ngmwryxUg6R3avHR9",
-    contracts: [
-      "4,999 PER MONTH - 1 MONTH ADVANCE 2 MONTH DEPOSITS"
-    ],
-    landmark: "Walking distance to McDonald's and Jollibee Karangalan Gate 1. Near Eastwood, Ortigas, Makati, BGC, and QC.",
-  }
-};
+ const properties = {
+      "ECO (888)": {
+        name: "ECO (888)",
+        type: "Studio Unit",
+        location: "69 Matahimik Street Riverside 2 Subdivision Brgy. Sto. Domingo Cainta, Rizal.",
+        map: "https://maps.app.goo.gl/ZtoYCfw6VS1Q2Tiy7",
+        contracts: [
+          "5,399 PER MONTH 1 YEAR CONTRACT.<br> 1 MONTH ADVANCE 2 MONTHS DEPOSIT.",
+          "5,899 PER MONTH 6 MONTHS CONTRACT.<br> 1 MONTH ADVANCE 2 MONTHS DEPOSIT.",
+          "6,298 PER MONTH 3 MONTHS CONTRACT (3000) UTILITY DEPOSIT.",
+          "6,888 PER MONTH MONTHLY CONTRACT 1 MONTH ADVANCE.",
+        ],
+        landmark: "Boundary of Pasig and Cainta. Walking distance to Ortigas Extension & Robinson's (Big R) Cainta Junction. Near Ortigas, Makati, Eastwood and Cubao.",
+      },
+      "GREEN": {
+        name: "GREEN",
+        type: "Studio Unit",
+        location: "Malinis St. Brgy. Sto Domingo Cainta, 1900 Rizal",
+        map: "https://maps.app.goo.gl/ukNVYbjwcobdxfhMA",
+        contracts: [
+          "5,799 per month. Upper unit. 1 month advance, 2 months deposit",
+          "5,399 per month. Lower unit. 1 year contract. 1 month advance, 2 months deposit."
+        ],
+        landmark: "Boundary of Pasig and Cainta. Walking distance to Ortigas Extension & Robinson's (Big R) Cainta Junction. Near Ortigas, Makati, Eastwood and Cubao.",
+      },
+      "ADI 168": {
+        name: "ADI 168",
+        type: "Semi-furnished unit",
+        location: "Lt. 6 Blk. 15 Kayumangi St. Karangalan Village, San Isidro Cainta, Rizal",
+        map: "https://maps.app.goo.gl/EQkAxPWgDAhyknM79",
+        contracts: [
+          "Trial - 3,999 - 1 MONTH - 1k utility DEPOSIT",
+          "3,999 PER MONTH - 6 MONTH CONTRACT - 1 MONTH ADVANCE 1 MONTH DEPOSIT"
+        ],
+        landmark: "Walking distance to LRT Line 2 Marikina-Pasig Station. Near Cubao, Ortigas, Makati, and Eastwood.",
+      },
+      "BED AND BATH": {
+        name: "Bed and Bath",
+        type: "Bedspace unit",
+        location: "74 Monaco St, Ciudad Grande 2, Pasig City, 1611 Metro Manila",
+        map: "https://maps.app.goo.gl/hphkFhzp1XebbM849",
+        contracts: [
+          "299 - Daily Minimum of 3 days stay.",
+          "1 MONTH TRIAL -(Lower bed: 2,200, Upper bed: 1,999)",
+          "Extended 1 month trial (Lower bed: 2,500, Upper bed: 2,300)",
+          "3,100 - MONTHLY CONTRACT",
+        ],
+        landmark: "One ride to Ortigas, Mandaluyong, Makati, Eastwood, Libis, and BGC. Walking distance to SM East Ortigas, Super 8 Grocery, McDonald's, and banks.",
+      },
+      "DREAM (1 Bedroom)": {
+        name: "DREAM",
+        type: "1 Bedroom Unit",
+        location: "80 Monaco St. Cuidad Grande 2, Ortigas Extension Pasig City.",
+        map: "https://maps.app.goo.gl/AP35c9sccbpSmaqU8",
+        contracts: [
+          "1 BEDROOM - 10,200 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "10,700 PER MONTH - 6 MONTHS CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "11,200 PER MONTH - MONTHLY CONTRACT - 3000 UTILITY DEPOSIT",
+          "1,000 - DAILY RENTAL",
+        ],
+        landmark: "Walking distance from SM East Ortigas. Near Ortigas, Makati, BGC, Eastwood, Cubao, and Marikina.",
+      },
+      "DREAM (2 Bedroom)": {
+        name: "DREAM",
+        type: "2 Bedroom Unit",
+        location: "80 Monaco St. Cuidad Grande 2, Ortigas Extension Pasig City.",
+        map: "https://maps.app.goo.gl/AP35c9sccbpSmaqU8",
+        contracts: [
+          "2 BEDROOMS - 12,400 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "13,400 - MONTHLY - 3K UTILITY DEPOSIT",
+        ],
+        landmark: "Walking distance from SM East Ortigas. Near Ortigas, Makati, BGC, Eastwood, Cubao, and Marikina.",
+      },
+      "DREAM (Studio Unit)": {
+        name: "DREAM",
+        type: "Studio Unit",
+        location: "80 Monaco St. Cuidad Grande 2, Ortigas Extension Pasig City.",
+        map: "https://maps.app.goo.gl/AP35c9sccbpSmaqU8",
+        contracts: [
+          "STUDIO - 7,900 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "8,400 PER MONTH - 6 MONTHS CONTACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "8,900 PER MONTH - MONTHLY CONTRACT - 3K UTILITY DEPOSIT",
+          "700-DAILY"
+        ],
+        landmark: "Walking distance from SM East Ortigas. Near Ortigas, Makati, BGC, Eastwood, Cubao, and Marikina.",
+      },
+      "PENTHAUZ": {
+        name: "PENTHAUZ",
+        type: "Studio unit",
+        location: "80 Monaco St. Pasig City",
+        map: "https://maps.app.goo.gl/AP35c9sccbpSmaqU8",
+        contracts: [
+          "7,999 to 8,500 per month"
+        ],
+        landmark: "Walking distance from SM East Ortigas. Near Ortigas, Makati, BGC, Eastwood, Cubao, and Marikina.",
+      },
+      "PLEASANT": {
+        name: "PLEASANT",
+        type: "Studio unit",
+        location: "Mt. Pleasant St. Brgy. Sta. Elena Marikina City",
+        map: "https://maps.app.goo.gl/5D3Ke41BDTLFLHPh9",
+        contracts: [
+          "1A TO 1E - 4,999 PER MONTH - 1 YR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "5,400 PER MONTH - 6 MONTHS CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "5,999 - MONTHLY CONTRACT - 3K UTILITY DEPOSIT",
+          "700 - DAILY",
+          "UNIT 1F, 2L, 3R, AND 4X - 5,099 PER MONTH - 1 YR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "5,599 PER MONTH - 6 MONTHS CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "6,099 - MONTHLY CONTRACT - 3K UTILITY DEPOSIT"
+        ],
+        landmark: "Near LRT Line 2 Marikina-Pasig Station. 1 ride to Cubao, Makati, Ortigas, Eastwood, and Manila.",
+      },
+      "HOMEY": {
+        name: "HOMEY",
+        type: "Bedroom unit",
+        location: "Lot 1 Block. 3 Kanlaon St., DM 5, Cainta, Rizal",
+        map: "https://maps.app.goo.gl/VnyT7tHi4QXXRHXj9",
+        contracts: [
+          "1ST FLOOR FRONT STUDIO - 3,999 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "2ND FLOOR 1 BEDROOM - 6,000 PER MONTH - 1 YR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "3,4,5 - 5,500 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "#2 - 6,000 PER MONTH - 1 YEAR CONTRACT - 1 MONTH ADVANCE 2 MONTHS DEPOSIT",
+          "6,7,8 - 6,000 PER MONTH – 1 YEAR CONTRACT W/ AC PROVISION - 1 MONTH ADVANCE 2 MONTHS DEPOSIT"
+        ],
+        landmark: "Near Cainta Municipal Hall, SM Savemore, Puregold, markets. Accessible to Ortigas Ext., C6, BGC, and Pasig.",
+      },
+      "KALAYAAN 888": {
+        name: "KALAYAAN 888",
+        type: "Studio unit",
+        location: "Karangalan Gate 1 Kalayaan St. San Isidro Cainta, Rizal",
+        map: "https://maps.app.goo.gl/ngmwryxUg6R3avHR9",
+        contracts: [
+          "4,999 PER MONTH - 1 MONTH ADVANCE 2 MONTH DEPOSITS"
+        ],
+        landmark: "Walking distance to McDonald's and Jollibee Karangalan Gate 1. Near Eastwood, Ortigas, Makati, BGC, and QC.",
+      }
+    };
 
-// Input Elements
-const locationInput = document.getElementById("locationInput");
-const typeInput = document.getElementById("typeInput");
-const landmarkInput = document.getElementById("landmarkInput");
-const results = document.getElementById("results");
+    const locationInput = document.getElementById("locationInput");
+    const typeInput = document.getElementById("typeInput");
+    const landmarkInput = document.getElementById("landmarkInput");
+    const results = document.getElementById("results");
 
-let debounceTimer;
+    let debounceTimer;
 
-function triggerSearch() {
-  clearTimeout(debounceTimer);
-  debounceTimer = setTimeout(() => {
-    search(
-      locationInput.value.trim().toLowerCase(),
-      typeInput.value.trim().toLowerCase(),
-      landmarkInput.value.trim().toLowerCase()
-    );
-  }, 300);
-}
-
-[locationInput, typeInput, landmarkInput].forEach(input => {
-  input.addEventListener("input", triggerSearch);
-  input.addEventListener("keyup", e => {
-    if (e.key === "Enter") triggerSearch();
-  });
-});
-
-function highlightKeywords(text, keyword) {
-  if (!text || !keyword) return text;
-  const regex = new RegExp(`(${keyword})`, "gi");
-  return text.replace(regex, "<strong class='text-lime-600'>$1</strong>");
-}
-
-function search(locationKeyword, typeKeyword, landmarkKeyword) {
-  if (!locationKeyword && !typeKeyword && !landmarkKeyword) {
-    results.innerHTML = "";
-    return;
-  }
-
-  let resultTables = "";
-  let matchFound = false;
-
-  for (const [_, data] of Object.entries(properties)) {
-    const matchesLocation = locationKeyword === "" ||
-      data.location.toLowerCase().includes(locationKeyword);
-
-    const matchesType = typeKeyword === "" ||
-      data.type.toLowerCase().includes(typeKeyword);
-
-    const matchesLandmark = landmarkKeyword === "" ||
-      (data.landmark || "").toLowerCase().includes(landmarkKeyword);
-
-    if (matchesLocation && matchesType && matchesLandmark) {
-      matchFound = true;
-
-      const table = document.createElement("table");
-      table.className =
-        "min-w-full bg-white text-sm text-left text-gray-600 shadow rounded overflow-hidden mb-8";
-
-      const thead = `
-        <thead class="bg-lime-500 text-white">
-          <tr>
-            <th class="px-4 py-3 text-center">Property</th>
-            <th class="px-4 py-3 text-center">Type</th>
-            <th class="px-4 py-3 text-center">Location</th>
-            <th class="px-4 py-3 text-center">Details</th>
-            <th class="px-4 py-3 text-center">Landmark</th>
-          </tr>
-        </thead>
-      `;
-
-      const tbody = data.contracts
-        .map(contract => `
-          <tr class="border-b hover:bg-gray-50">
-            <td class="px-4 py-2 font-bold">${highlightKeywords(data.name, locationKeyword)}</td>
-            <td class="px-4 py-2">${highlightKeywords(data.type, typeKeyword)}</td>
-            <td class="px-4 py-2">
-              ${highlightKeywords(data.location, locationKeyword)}<br>
-              <a href="${data.map}" target="_blank" class="text-blue-600 underline">${data.map}</a>
-            </td>
-            <td class="px-4 py-2">${contract}</td>
-            <td class="px-4 py-2">${highlightKeywords(data.landmark || "", landmarkKeyword)}</td>
-          </tr>
-        `)
-        .join("");
-
-      table.innerHTML = thead + `<tbody>${tbody}</tbody>`;
-      resultTables += table.outerHTML;
+    function triggerSearch() {
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => {
+        search(
+          locationInput.value.trim().toLowerCase(),
+          typeInput.value.trim().toLowerCase(),
+          landmarkInput.value.trim().toLowerCase()
+        );
+      }, 300);
     }
-  }
 
-  results.innerHTML = matchFound
-    ? resultTables
-    : '<div class="text-center text-gray-500 mt-4">No matching properties found.</div>';
-}
+    [locationInput, typeInput, landmarkInput].forEach(input => {
+      input.addEventListener("input", triggerSearch);
+      input.addEventListener("keyup", e => {
+        if (e.key === "Enter") triggerSearch();
+      });
+    });
+
+    function highlightKeywords(text, keyword) {
+      if (!text || !keyword || keyword === "all") return text;
+      const regex = new RegExp(`(${keyword})`, "gi");
+      return text.replace(regex, "<strong class='text-lime-600'>$1</strong>");
+    }
+
+    function containsAllWord(k) {
+      if (!k) return false;
+      return k.split(/\s+/).some(w => w === "all");
+    }
+
+    function search(locationKeyword, typeKeyword, landmarkKeyword) {
+      if (!locationKeyword && !typeKeyword && !landmarkKeyword) {
+        results.innerHTML = "";
+        return;
+      }
+
+      let resultTables = "";
+      let matchFound = false;
+
+      const anyAll = containsAllWord(locationKeyword) || containsAllWord(typeKeyword) || containsAllWord(landmarkKeyword);
+      const allFilled = locationKeyword && typeKeyword && landmarkKeyword;
+      const showAll = anyAll || allFilled;
+
+      for (const [_, data] of Object.entries(properties)) {
+        const matchesLocation = showAll || locationKeyword === "" || data.location.toLowerCase().includes(locationKeyword);
+        const matchesType = showAll || typeKeyword === "" || data.type.toLowerCase().includes(typeKeyword);
+        const matchesLandmark = showAll || landmarkKeyword === "" || (data.landmark || "").toLowerCase().includes(landmarkKeyword);
+
+        if (matchesLocation && matchesType && matchesLandmark) {
+          matchFound = true;
+
+          const table = document.createElement("table");
+          table.className = "min-w-full bg-white text-sm text-left text-gray-600 shadow rounded overflow-hidden mb-8";
+
+          const thead = `
+            <thead class="bg-lime-500 text-white">
+              <tr>
+                <th class="px-4 py-3 text-center">Property</th>
+                <th class="px-4 py-3 text-center">Type</th>
+                <th class="px-4 py-3 text-center">Location</th>
+                <th class="px-4 py-3 text-center">Details</th>
+                <th class="px-4 py-3 text-center">Landmark</th>
+              </tr>
+            </thead>
+          `;
+
+          const tbody = data.contracts
+            .map(contract => `
+              <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-2 font-bold">${highlightKeywords(data.name, locationKeyword)}</td>
+                <td class="px-4 py-2">${highlightKeywords(data.type, typeKeyword)}</td>
+                <td class="px-4 py-2">
+                  ${highlightKeywords(data.location, locationKeyword)}<br>
+                  <a href="${data.map}" target="_blank" class="text-blue-600 underline">${data.map}</a>
+                </td>
+                <td class="px-4 py-2">${contract}</td>
+                <td class="px-4 py-2">${highlightKeywords(data.landmark || "", landmarkKeyword)}</td>
+              </tr>
+            `)
+            .join("");
+
+          table.innerHTML = thead + `<tbody>${tbody}</tbody>`;
+          resultTables += table.outerHTML;
+        }
+      }
+
+      results.innerHTML = matchFound
+        ? resultTables
+        : '<div class="text-center text-gray-500 mt-4">No matching properties found.</div>';
+    }
